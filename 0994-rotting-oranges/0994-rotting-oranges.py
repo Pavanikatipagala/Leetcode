48f3ec5a422c68700393f13cf1ac96a73db078a7
Pavanikatipagala <kpavani1023@gmail.com>
2026-07-07 14:59:20 +0530

    Time: 12 ms (7.22%), Space: 12.3 MB (58.36%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,38 +1,48 @@
 class Solution(object):
     def orangesRotting(self, grid):
-        rows = len(grid)
-        cols = len(grid[0])
+        m = len(grid)
+        n = len(grid[0])
 
-        q= deque()
-        fresh =0
-
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 2:
-                    q.append((r,c))
-                elif grid[r][c] == 1:
-                    fresh +=1
+        queue=[]
+        fresh = 0
         
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    fresh += 1
+
+                elif grid[i][j] == 2:
+                    queue.append([i,j])
+        directions = [[-1,0],[0,1],[1,0],[0,-1]]
+        mins = 0
+        while queue and fresh > 0:
+            size = len(queue)
+            mins +=1
+            for i in range(size):
+                curr = queue.pop(0)
+                row = curr[0]
+                col = curr[1]
+
+                for dir in directions:
+                    nr = row + dir[0]
+                    nc = col + dir[1]
+
+                    if nr< 0 or nr>=m or nc<0 or nc>=n:
+                        continue
+                    
+                    if grid[nr][nc] == 0 or grid[nr][nc] == 2:
+                        continue
+                    
+                    fresh -= 1
+                    grid[nr][nc] = 2
+                    queue.append([nr,nc])
         if fresh == 0:
-            return 0
-        
-        directions=[(1,0),(-1,0),(0,1),(0,-1)]
+            return mins
+        return -1
 
-        minutes = 0
-        while q and fresh >0:
-            size = len(q)
-            for _ in range(size):
-                r,c = q.popleft()
 
-                for dr, dc in directions:
-                    nr =r+dr
-                    nc=c+dc
 
-                    if (0<=nr< rows and 0<=nc<cols and grid[nr][nc]==1):
-                        grid[nr][nc]=2
-                        fresh -=1
-                        q.append((nr,nc))
-            minutes +=1
-        return minutes if fresh == 0 else -1
+
+
 
         
