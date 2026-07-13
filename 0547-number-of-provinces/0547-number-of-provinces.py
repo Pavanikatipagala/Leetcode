52d47397d9f6764bfbb6e1b9d5420399761947e0
Pavanikatipagala <kpavani1023@gmail.com>
2026-07-13 14:30:26 +0530

    Time: 0 ms (100%), Space: 20.2 MB (88.9%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,18 +1,39 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        n = len(isConnected)
-        visited = [False] * n
-        count = 0
+        n=len(isConnected)
+        parent=[i for i in range(n)]
+        rank =[0 for i in range(n)]
+
 
         for i in range(n):
-            if not visited[i]:
-                self.dfs(i,visited,isConnected)
-                count +=1
-        return count
-    
-    def dfs(self,node,visited,isConnected):
-        n = len(isConnected)
-        visited[node]=True
-        for neighbor in range(n):
-            if not visited[neighbor] and isConnected[node][neighbor]==1:
-                self.dfs(neighbor,visited,isConnected)
+            for j in range(i+1,n):
+                if isConnected[i][j]==1:
+                    self.union(i,j,parent,rank)
+        province =0
+        for  i in range(n):
+            if self.find(i,parent)==i:
+                province+=1
+        return province
+    def union(self,u,v,parent,rank):
+        pu = self.find(u,parent)
+        pv = self.find(v,parent)
+
+        if pu==pv:
+            return
+
+            
+        if rank[pu]>rank[pv]:
+            parent[pv]=pu
+        elif rank[pv]>rank[pu]:
+            parent[pu]=pv
+        else:
+            parent[pv]=pu
+            rank[pu]+=1
+       
+                    
+    def find(self,x,parent):
+        if parent[x]==x:
+            return x
+        parent[x] = self.find(parent[x],parent)
+        return parent[x]
+
